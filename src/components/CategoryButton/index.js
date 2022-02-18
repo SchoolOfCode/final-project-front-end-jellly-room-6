8d575ef2ba0630db04917d024c1d92e690f55fe4
Laurence279 <lnunn279@gmail.com>
2022-02-18 12:02:32 +0000

    Add paths

--- src/components/CategoryButton/index.js
+++ src/components/CategoryButton/index.js
@@ -2,7 +2,12 @@ import Link from 'next/link'
 
 export default function CategoryButton({category, handleClick}){
     return (
-    <Link href={`/question`}>
+    <Link href={{
+        pathname: "/question",
+        query: {
+            category
+        }
+    }}>
 
     <a>
     <button onClick={handleClick}>{category}</button>
