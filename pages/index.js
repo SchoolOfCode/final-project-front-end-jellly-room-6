f84ffe3b179991f9bfbc26409616701c859069e9
Tiger Fingers <loreen@anet360.com>
2022-02-18 09:57:28 +0000

    small edits to landing page css

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,20 @@ import NavBar from "../src/components/NavBar";
 import Home from "./home";
 import Button from "react-bootstrap/Button"
 import Image from "next/image";
+import css from "../styles/index.module.css";
+
 export default function LandingPage() {
   return (
     <div>
       
      
-<Button>LogIn</Button>
-<h1>JELLLY</h1>
+<Button className={css.login}>LogIn</Button>
+<div className={css.wording}>  
+<h1 className={css.title}>JELLLY</h1>
+</div>
 <Image src="/logoJelly.png" alt="me" width="64" height="64" />
-<h2>The fun, free way to learn maths and improve financial literacy</h2>
-<Button>Get Started</Button>
+<h2 className={css.text}>The fun, free way to learn maths and improve financial literacy</h2>
+<Button className={css.start}>Get Started</Button>
 <Image src="/threeJellies.png" alt="me" width="64" height="64"/>
 
 
