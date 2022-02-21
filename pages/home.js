502b22974c9cb73ab1555783403d519197af849b
LTBurton1 <Liam.burton@hotmail.com>
2022-02-21 16:02:20 +0000

    Quiz working

--- pages/home.js
+++ pages/home.js
@@ -1,154 +1,110 @@
 // Import Header here
 import { useState, useEffect, useLayoutEffect } from "react";
 import styles from "../styles/home.module.css";
-import NavBar from "../src/components/NavBar"
+import NavBar from "../src/components/NavBar";
 import CategoryButton from "../src/components/CategoryButton";
-import { useUser, getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useUser, getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 //Plan
 //-onClick go to questions (loading page, etc.)
 //-stats: we need to fetch user info.
 //states for the user: {xp, beans}
 //state for categories:[""]
 
-
-
-export default function Home ({authenticatedUser}) {
-
+export default function Home({ authenticatedUser }) {
+  const categories = ["Addition", "Subtraction", "Multiplication"];
   const { user, error, isLoading } = useUser();
-  
-
-
   const [username, setUsername] = useState(authenticatedUser);
-
   const [userInfo, setUserInfo] = useState("");
 
-  const [categories,setCategories] = useState([]);
-
-
   useEffect(() => {
-    
-    async function createNewUser(username){
-      const res = await fetch (`https://jellly.herokuapp.com/users`,{
+    async function createNewUser(username) {
+      const res = await fetch(`https://jellly.herokuapp.com/users`, {
         method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-      },
-        body: JSON.stringify({username})
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ username }),
       });
       const data = await res.json();
-      if(!data.payload || data.payload.length === 0) return;
-      console.log("Created user:", data.payload);
+      if (!data.payload || data.payload.length === 0) return;
+      // console.log("Created user:", data.payload);
       await fetchUser();
     }
-    
-    async function fetchUser(){
-      if(!authenticatedUser) return;
-      console.log("Retrieving user " + authenticatedUser)
+
+    async function fetchUser() {
+      if (!authenticatedUser) return;
+      // console.log("Retrieving user " + authenticatedUser);
       const res = await fetch(`https://jellly.herokuapp.com/users/${authenticatedUser}`);
       const data = await res.json();
-      console.log(data)
-      if(!data.payload || data.payload.length === 0) {
-        console.log("User not found.. creating user: " + authenticatedUser)
+      console.log(data);
+      if (!data.payload || data.payload.length === 0) {
+        // console.log("User not found.. creating user: " + authenticatedUser);
         createNewUser(authenticatedUser);
         return;
       }
-      console.log("Found user:", data.payload)
-      setUserInfo(data.payload[0])
+      // console.log("Found user:", data.payload);
+      setUserInfo(data.payload[0]);
     }
 
     fetchUser();
-
-
-  }, [authenticatedUser])
-  
-  
-    
-
- 
+  }, [authenticatedUser]);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-  if (!user){
-    window.location.href = "/"
+  if (!user) {
+    window.location.href = "/";
   }
-    return (
-      user && (
-    
+  return (
+    user && (
       <div>
-      
-        <NavBar/>
+        <NavBar />
         <div className={styles.grid}>
-
-        <div className={`${styles.gridItem} ${styles.statsDisplay}`}>
-                <h2>{userInfo.username}</h2>
-                <h2>Beans: {username}</h2>
-                <p>Level 1</p>
-                <p>{userInfo.xp} XP</p>
+          <div className={`${styles.gridItem} ${styles.statsDisplay}`}>
+            <h2>{userInfo.username}</h2>
+            <h2>Beans: {username}</h2>
+            <p>Level 1</p>
+            <p>{userInfo.xp} XP</p>
           </div>
 
           <div className={`${styles.gridItem} ${styles.gridItemLevel}`}>
-
-
-
             <div className={styles.level}>
-                <div className={styles.levelContainer}>
-
-
-                  <CategoryButton category="Addition"/>
-                  <CategoryButton category="Subtraction"/>
-                  <CategoryButton category="Multiplication"/>
-
-                  
-                  
-                  
-                  
-                </div>
+              <div className={styles.levelContainer}>
+                {categories.map(category => (
+                  <CategoryButton key={category} category={category} />
+                ))}
+              </div>
             </div>
-
           </div>
-
-              
         </div>
-
-
       </div>
-      )
-    );
-  }
+    )
+  );
+}
 
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    const session = getSession(ctx.req, ctx.res);
 
-  export const getServerSideProps = withPageAuthRequired({
-     async getServerSideProps(ctx) {
-      const session = getSession(ctx.req, ctx.res);
+    const getUsername = async () => {
+      const domain = "jelllyapp.eu.auth0.com";
 
-      const getUsername = async () => {
+      try {
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${session.user.sub}`;
 
-        const domain = "jelllyapp.eu.auth0.com";
-        
-        try {
-    
-          const userDetailsByIdUrl = `https://${domain}/api/v2/users/${session.user.sub}`;
-    
-          const metadataResponse = await fetch(userDetailsByIdUrl, {
-            headers: {
-              Authorization: `Bearer ${session.accessToken}`
-             },
-          });
-    
-          const data = await metadataResponse.json();
-          return data.username
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`,
+          },
+        });
 
-        } catch (e) {
-          console.log(e.message);
-        }
-  
-      };
-      const username = await getUsername();
-      return { props: 
-        { authenticatedUser: username || "No user"} 
-      };
-     }
-    });
-  
-
+        const data = await metadataResponse.json();
+        return data.username;
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    const username = await getUsername();
+    return { props: { authenticatedUser: username || "No user" } };
+  },
+});
--- pages/question.js
+++ pages/question.js
@@ -1,50 +1,97 @@
-import { useRouter } from 'next/router'
-import { useUser } from '@auth0/nextjs-auth0';
+import { useEffect, useState } from "react";
+import { useUser } from "@auth0/nextjs-auth0";
 import NavBar from "../src/components/NavBar";
+import Results from "../src/components/Results";
 
+export default function Question({ questions }) {
+  const { user, error, isLoading } = useUser();
+  const [questionCount, setQuestionCount] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
+  const [score, setScore] = useState(0);
+  const [win, setWin] = useState(false);
+  const [complete, setComplete] = useState(false);
 
+  useEffect(() => {
+    setCurrentQuestion(questions[questionCount]);
+  }, [questionCount]);
 
-export default function Question({category, question}){
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+  if (!user) {
+    window.location.href = "/";
+  }
 
-    const { user, error, isLoading } = useUser();
-     
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>{error.message}</div>;
-    if (!user){
-      window.location.href = "/"
-    }
+  function checkAnswer(e) {
+    if (e.target.textContent == currentQuestion.correct) setScore(score + 1);
+    else console.log("incorrect");
+    if (questionCount < questions.length - 1) return setQuestionCount(questionCount + 1);
+    calculateScore();
+  }
 
-    const correctAnswer = question.answer;
+  function calculateScore() {
+    if (score >= questions.length * 0.5) setWin(true);
+    else setWin(false);
+    setComplete(true);
+  }
 
-    function handleClick(event){
-
-        if(event.target.value !== String(correctAnswer)) {
-            console.log("Try Again")
-            return
-        }
-        console.log("Correct")
-    }
-    
-    return user && (<div>
-        <NavBar/>
-        <h1>Put {category} question here</h1>
-        <h1>{question.question}</h1>
-        <button onClick={handleClick} value="1">1</button>
-        <button onClick={handleClick} value="3">3</button>
-        <button onClick={handleClick} value="5">5</button>
-        <button onClick={handleClick} value="10">10</button>
-    </div>)
+  return (
+    user && (
+      <div>
+        <NavBar />
+        {!complete && (
+          <div>
+            <h2>{currentQuestion.question}</h2>
+            <button onClick={checkAnswer}>{currentQuestion.answers[0]}</button>
+            <button onClick={checkAnswer}>{currentQuestion.answers[1]}</button>
+            <button onClick={checkAnswer}>{currentQuestion.answers[2]}</button>
+            <button onClick={checkAnswer}>{currentQuestion.answers[3]}</button>
+          </div>
+        )}
+        {complete && <Results hasWon={win} />}
+      </div>
+    )
+  );
 }
 
-
 export async function getServerSideProps(context) {
-    return {
-        props: {
-            category: context.query.category,
-            question: {
-                question: "Ariel was playing basketball. 1 of her shots went in the hoop. 2 of her shots did not go in the hoop. How many shots were there in total?",
-                answer: 3
-            }
-        }
-    }
+  const questions = [
+    {
+      category: "Addition",
+      question:
+        "Ariel was playing basketball. 1 of her shots went in the hoop. 2 of her shots did not go in the hoop. How many shots were there in total?",
+      answers: [3, 2, 5, 6],
+      correct: 3,
+    },
+    {
+      category: "Addition",
+      question:
+        "Adrianna has 10 pieces of gum to share with her friends. There wasn't enough gum for all her friends, so she went to the store to get 3 more pieces of gum. How many pieces of gum does Adrianna have now?",
+      answers: [10, 7, 15, 13],
+      correct: 13,
+    },
+    {
+      category: "Addition",
+      question:
+        "Ariel was playing basketball. 1 of her shots went in the hoop. 2 of her shots did not go in the hoop. How many shots were there in total?",
+      answers: [3, 2, 5, 6],
+      correct: 3,
+    },
+    {
+      category: "Addition",
+      question:
+        "Adrianna has 10 pieces of gum to share with her friends. There wasn't enough gum for all her friends, so she went to the store to get 3 more pieces of gum. How many pieces of gum does Adrianna have now?",
+      answers: [10, 7, 15, 13],
+      correct: 13,
+    },
+  ];
+
+  const questionsByCategory = questions.filter(
+    question => question.category === context.query.category
+  );
+
+  return {
+    props: {
+      questions: questionsByCategory,
+    },
+  };
 }
