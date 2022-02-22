7aeafc1bd5fec80161da8e66d92f32402ad3f3cc
Laurence279 <lnunn279@gmail.com>
2022-02-22 15:29:10 +0000

    add icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
+
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.ico"/>
+      </Head>
       <Component {...pageProps} />
+
     </UserProvider>
   );
 }
