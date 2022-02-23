731076c9d893bfe4af0322899fbd60503dbec079
LTBurton1 <Liam.burton@hotmail.com>
2022-02-23 10:29:07 +0000

    Centered layout and Get Started Button styled

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import NavBar from "../src/components/NavBar";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 import Image from "next/image";
-import css from "../styles/index.module.css";
-import { useUser } from '@auth0/nextjs-auth0';
+import styles from "../styles/index.module.css";
+import { useUser } from "@auth0/nextjs-auth0";
 import Link from "next/link";
 
 export default function LandingPage() {
@@ -11,30 +10,28 @@ export default function LandingPage() {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
-  if(user){
-    window.location.href = "/home"
+  if (user) {
+    window.location.href = "/home";
   }
 
   return (
-    
-    <div>
-      
-      <hr/>
+    <div className={styles.container}>
+      <hr />
       <Link href="/api/auth/login">
-      <a><Button className={css.login}>LogIn</Button></a>
+        <a>
+          <Button className={styles.login}>Log In</Button>
+        </a>
       </Link>
 
-      <div className={css.wording}>  
-      <h1 className={css.title}>JELLLY</h1>
+      <div className={styles.wording}>
+        <h1 className={styles.title}>JELLLY</h1>
       </div>
       <Image src="/logoJelly.png" alt="me" width="64" height="64" />
-      <h2 className={css.text}>The fun, free way to learn maths and improve financial literacy</h2>
-      <Button className={css.start}>Get Started</Button>
-      <Image src="/threeJellies.png" alt="me" width="64" height="64"/>
-
-
-      
+      <h2 className={styles.text}>
+        The fun, free way to learn maths and improve financial literacy
+      </h2>
+      <button className="btn large">Get Started</button>
+      <Image src="/threeJellies.png" alt="me" width="64" height="64" />
     </div>
-    
   );
 }
