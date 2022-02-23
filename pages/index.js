904a55bb4ab5c5a1d8af44f2bdaf3fa66cf3133d
LTBurton1 <Liam.burton@hotmail.com>
2022-02-23 14:22:12 +0000

    Button hover effects

--- pages/index.js
+++ pages/index.js
@@ -16,25 +16,37 @@ export default function LandingPage() {
 
   return (
     <div className={styles.container}>
-      
-      <Link href="/api/auth/login">
-        <a>
-          <Button className="btn login">Login</Button>
-        </a>
-      </Link>
+      <header className={styles.header}>
+        <Link href="/api/auth/login">
+          <a>
+            <Button className="btn login">Login</Button>
+          </a>
+        </Link>
+      </header>
 
-      <div className={styles.titleContainer}>
-        <Image className={styles.jellyBean} src="/logoJelly.png" alt="me" width="100" height="100" />
-        <h1 className={styles.title}>JELLLY</h1>
+      <main className={styles.main}>
+        <div className={styles.titleContainer}>
+          <Image
+            className={styles.jellyBean}
+            src="/logoJelly.png"
+            alt="me"
+            width="100"
+            height="100"
+          />
+          <h1 className={styles.title}>JELLLY</h1>
+        </div>
+
+        <h2 className={styles.text}>
+          The fun, free way to learn maths and improve financial literacy.
+        </h2>
+        <button className="btn large" id={styles.getStarted}>
+          Get Started
+        </button>
+      </main>
+      <div className={styles.jellies}>
+        <Image src="/threeJellies.png" alt="me" width="200" height="200" />
       </div>
-
-      <h2 className={styles.text}>
-        The fun, free way to learn maths and improve financial literacy
-      </h2>
-      <button className="btn large" id={styles.getStarted}>Get Started</button>
-      <div className={styles.jellies}><Image src="/threeJellies.png" alt="me" width="150" height="150" /></div>
-      <hr className={styles.line}/>    
+      <hr className={styles.line} />
     </div>
-
   );
 }
