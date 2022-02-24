435a422ee74a021f58591597f2dfaef1f7e81c60
YeganaAbd <abd.yegane@gmail.com>
2022-02-24 13:00:11 +0000

    border for statistics

--- src/components/Profile/StatisticsItem/index.js
+++ src/components/Profile/StatisticsItem/index.js
@@ -1,12 +1,10 @@
+import styles from "./statistics.module.css";
 
-
-
-export default function StatisticsItem({title, value}){
-
-
-    return <div className="statistics-item">
-    <h3>{title}</h3>
-    <div>{value}</div>
-  </div>
-
+export default function StatisticsItem({ title, value }) {
+  return (
+    <div className="statistics-item">
+      <h3>{title}</h3>
+      <div className={styles.statisticsValue}>{value}</div>
+    </div>
+  );
 }
