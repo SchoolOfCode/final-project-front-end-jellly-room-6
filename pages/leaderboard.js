676540de3feaa740e5cbc3f4ba4cdde50725dfb0
LTBurton1 <Liam.burton@hotmail.com>
2022-02-24 11:23:11 +0000

    Leaderboard displaying user info

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,15 +1,45 @@
-import { useUser } from '@auth0/nextjs-auth0';
+import { useUser } from "@auth0/nextjs-auth0";
+import { useState } from "react";
 import NavBar from "../src/components/NavBar";
 
-export default function Leaderboard(){
-    const { user, error, isLoading } = useUser();
-  
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>{error.message}</div>;
-    if (!user){
-      window.location.href = "/"
-    }
-    return user && (<div>
-        <NavBar/>
-        Leaderboard</div>)
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export default function Leaderboard({ users }) {
+  const { user, error, isLoading } = useUser();
+  const [usersList, setUsersList] = useState(users);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+  if (!user) {
+    window.location.href = "/";
+  }
+  return (
+    user && (
+      <div>
+        <NavBar />
+        <h1>Leaderboard</h1>
+        <ol>
+          {users.map(user => (
+            <li key={user}>
+              <h2>{user.username}</h2>
+              <h3>{user.xp}</h3>
+            </li>
+          ))}
+        </ol>
+      </div>
+    )
+  );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch(`${API_URL}/users`);
+  const data = await response.json();
+
+  const users = data.payload.sort((a, b) => (a.xp < b.xp ? 1 : -1));
+
+  return {
+    props: {
+      users,
+    },
+  };
 }
