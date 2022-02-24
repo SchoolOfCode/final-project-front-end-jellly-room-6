0cd9700812600a68723963b0b255c140a15436f5
YeganaAbd <abd.yegane@gmail.com>
2022-02-24 14:54:38 +0000

    statistics

--- src/components/Profile/Badge/index.js
+++ src/components/Profile/Badge/index.js
@@ -2,7 +2,7 @@ import styles from "./badge.module.css";
 
 export default function Badge({ name }) {
   return (
-    <div className={`${styles.badgebtn} btn`}>
+    <div className={`${styles.badgebtn}`}>
       <p>{name}</p>
     </div>
   );
