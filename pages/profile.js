10f41c77131eaef19db75fa5d81ddcbf98a81e73
YeganaAbd <abd.yegane@gmail.com>
2022-02-24 12:50:30 +0000

    Styling of profile

--- pages/profile.js
+++ pages/profile.js
@@ -4,11 +4,10 @@ import Loading from "../src/components/Loading";
 import Image from "next/image";
 import StatisticsItem from "../src/components/Profile/StatisticsItem";
 import Badge from "../src/components/Profile/Badge";
-import styles from "../styles/profile.module.css"
+import styles from "../styles/profile.module.css";
 
-
-export default function Profile({userID}) {
-  console.log(userID)
+export default function Profile({ userID }) {
+  console.log(userID);
   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <Loading>Loading...</Loading>;
@@ -17,36 +16,45 @@ export default function Profile({userID}) {
     window.location.href = "/";
   }
 
-  
-
-
   return (
     user && (
       <div>
         <NavBar />
-        <div>
-          <div> 
-
-            <Image src="/logoJelly.png" width={50} height={50} />
+        <div className={styles.profileContainer}>
+          <div className={styles.userDetails}>
+            <Image src="/logoJelly.png" alt="Jelly" width={50} height={50} />
             <h2>Bob Smith</h2>
             <h3>Bobbles123</h3>
-
           </div>
 
-          <div>
-            <h2>Statistics</h2>
-            <StatisticsItem title="Player Level" value="2" />
-            <StatisticsItem title="Total XP" value="145" />
-            <StatisticsItem title="Total Beans" value="20" />
-            <StatisticsItem title="Leaderboard Position" value="1" />
+          <h2 className={styles.title}>Statistics</h2>
+          <div className={styles.statistics}>
+            <StatisticsItem
+              className={styles.StatisticsItem}
+              title="Player Level"
+              value="2"
+            />
+            <StatisticsItem
+              className={styles.StatisticsItem}
+              title="Total XP"
+              value="145"
+            />
+            <StatisticsItem
+              className={styles.StatisticsItem}
+              title="Total Beans"
+              value="20"
+            />
+            <StatisticsItem
+              className={styles.StatisticsItem}
+              title="Leaderboard Position"
+              value="1"
+            />
           </div>
-          <div>
-            <h2>Achievements</h2>
-              <Badge name="Level 1"/>
-              <Badge name="Level 2"/>
-              <Badge name="Level 3"/>
-            
-
+          <h2 className={styles.title}>Achievements</h2>
+          <div className={styles.badge}>
+            <Badge className={styles.badgeItem} name="Level 1" />
+            <Badge className={styles.badgeItem} name="Level 2" />
+            <Badge className={styles.badgeItem} name="Level 3" />
           </div>
         </div>
       </div>
@@ -55,11 +63,10 @@ export default function Profile({userID}) {
 }
 
 // Context gives us access to queries in the URL I.E. /questions?category=Addition
-export async function getServerSideProps(context) {
-
-  return {
-    props: {
-      userID: context.query.userId
-    },
-  };
-}
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       userID: context.query.userId,
+//     },
+//   };
+// }
