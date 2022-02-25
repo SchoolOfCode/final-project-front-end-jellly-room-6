c54a657e6c8ba809d0e57b5b61d9c45e61a339a8
YeganaAbd <abd.yegane@gmail.com>
2022-02-25 15:15:00 +0000

    badge button changed

--- src/components/BeanButton/index.js
+++ src/components/BeanButton/index.js
@@ -5,7 +5,7 @@ import styles from "./BeanButton.module.css";
 export default function BeanButton({ text, color }) {
   return (
     <button
-      className={`${styles.beanButton}`}
+      className={styles.beanButton}
       style={{ filter: `hue-rotate(${color}deg)` }}
     >
       <p className={styles.category}>{text}</p>
