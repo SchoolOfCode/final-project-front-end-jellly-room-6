0165f91bd0e21858f03f3d23dae31908129b03bd
Laurence279 <lnunn279@gmail.com>
2022-02-25 14:22:07 +0000

    merge bean button

--- src/components/Home/CategoryButton/index.js
+++ src/components/Home/CategoryButton/index.js
@@ -2,11 +2,7 @@ import Link from "next/link";
 import BeanButton from "../../BeanButton";
 import styles from "./index.module.css";
 
-<<<<<<< HEAD
-export default function CategoryButton({ category, isCompleted }) {
-=======
-export default function CategoryButton({ category, user, color, isCompleted }) {
->>>>>>> 6735c33834905ea86a71681c390b3abfed2fab99
+export default function CategoryButton({ category, isCompleted, color }) {
   return (
     <Link
       href={{
@@ -17,13 +13,7 @@ export default function CategoryButton({ category, user, color, isCompleted }) {
       }}
     >
       <a>
-<<<<<<< HEAD
-        <button
-          className={`${styles.button} ${isCompleted && styles.completed}`}
-        >
-=======
         <BeanButton color={color} text={category} disabled={isCompleted ? true : false}>
->>>>>>> 6735c33834905ea86a71681c390b3abfed2fab99
           {category}
         </BeanButton>
       </a>
--- src/components/Home/CategoryContainer/index.js
+++ src/components/Home/CategoryContainer/index.js
@@ -2,7 +2,7 @@ import styles from "../../../../styles/home.module.css";
 import CategoryButton from "../CategoryButton";
 import CategoryDropdown from "../CategoryDropdown";
 
-export default function CategoryContainer({ id, categories, userId, completedCategories }) {
+export default function CategoryContainer({ handleSelect, categories, userId, completedCategories }) {
   const buttonColors = categories.map(() => Math.floor(Math.random() * 720));
 
   return (
