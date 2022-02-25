fac52ecdde7751de9a4c8a68baaff2e83d485913
shuamp <castel.davila@gmail.com>
2022-02-25 12:01:20 +0000

    shop page visuale done

--- src/components/ShopCategory/index.js
+++ src/components/ShopCategory/index.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import ShopItem from '../ShopItem/ShopItem'
-import style from "../../../styles/shop.module.css"
+import React from "react";
+import ShopItem from "../ShopItem/ShopItem";
+import style from "../../../styles/shop.module.css";
 
-export default function ShopCategory({categorytitle, data}) {
+export default function ShopCategory({ categorytitle, data }) {
   return (
-    <div>
-    <div className={style.categorytitle}><h3>{categorytitle}</h3></div>
-    <div className={style.category}>
-        {data.map((i) => <ShopItem key={i.id} src={i.src} alt={i.alt} price={i.price}/> )}
+    <div className={style.shopcategory}>
+      <div className={style.categorytitle}>
+        <h3>{categorytitle}</h3>
+      </div>
+      <div className={style.category}>
+        {data.map((i) => (
+          <ShopItem key={i.id} src={i.src} alt={i.alt} price={i.price} />
+        ))}
+      </div>
     </div>
-    </div>
-  )
-};
-
+  );
+}
--- src/components/ShopItem/ShopItem.js
+++ src/components/ShopItem/ShopItem.js
@@ -6,10 +6,10 @@ export default function ShopItem({ src, alt, price }) {
     
   return (
     <div className={style.shopitem}>
-      <Image src={src} width={50} height={80} alt={alt} />
+      <Image src={src} width={50} height={100} alt={alt} />
       <div className={style.price}>
       <p className={style.tagprice}> {price}</p>
-      <Image src="/beansCoins.png" width={50} height={8} alt="beans-coins" />
+      <Image src="/beansCoins.png" width={50} height={2} alt="beans-coins" />
       </div>
       <Button>Buy</Button>
     </div>
