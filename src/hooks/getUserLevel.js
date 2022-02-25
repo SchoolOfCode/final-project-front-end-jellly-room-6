aa91925fcc086a82ee4e069620f2efd9736868e8
Laurence279 <lnunn279@gmail.com>
2022-02-25 17:03:40 +0000

    add progress bar

--- src/hooks/getUserLevel.js
+++ src/hooks/getUserLevel.js
@@ -11,6 +11,48 @@ const levelRequirements = {
   10: 5000,
 };
 
+
+export function getUserRemainingXpPercentage(xp, level){
+
+  const difference = levelRequirements[level] - levelRequirements[level-1]
+  //Difference in xp between current level and next level
+
+  const xpLessLevel = xp - levelRequirements[level-1];
+  //Gets the xp earned since the start of this level.
+
+  //Work out percentage of current xp to remaining xp required
+  const percentage = xpLessLevel / difference;
+
+  return Number(percentage.toFixed(2))
+}
+
+export function getUserRemainingXp(xp, level){
+  
+  const difference = levelRequirements[level] - levelRequirements[level-1]
+  //Difference in xp between current level and next level
+
+  const xpLessLevel = xp - levelRequirements[level-1];
+  //Gets the xp earned since the start of this level.
+
+  //Work out percentage of current xp to remaining xp required
+  const remainingXp = difference - xpLessLevel;
+
+  return remainingXp;
+}
+
+export function getUserXpToNextLevel(level){
+  return levelRequirements[level];
+}
+
+export function getUserXpEarnedThisLevel(xp, level){
+  return xp - levelRequirements[level-1];
+}
+
+export function getUserXpNextLevelRequired(level){
+  return levelRequirements[level] - levelRequirements[level-1]
+}
+
+
 export default function getUserLevel(xp) {
   for (let level in levelRequirements) {
     if (xp <= levelRequirements[level]) {
