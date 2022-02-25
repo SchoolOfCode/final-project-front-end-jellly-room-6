e57eb1090db92ddf60c8d4f1c01cdbdbe8bed155
LTBurton1 <Liam.burton@hotmail.com>
2022-02-25 14:05:11 +0000

    Removed disabled prop and fixed problems

--- src/components/BeanButton/index.js
+++ src/components/BeanButton/index.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./BeanButton.module.css";
 
-export default function BeanButton({ text, color, disabled }) {
+export default function BeanButton({ text, color }) {
   return (
-    <button
-      disabled={disabled}
-      className={`${styles.beanButton} ${disabled && styles.disabled}`}
-      style={{ filter: `hue-rotate(${color}deg)` }}
-    >
+    <button className={styles.beanButton} style={{ filter: `hue-rotate(${color}deg)` }}>
       <p className={styles.category}>{text}</p>
-      <img className={styles.jellybean} src="jellybean-button.svg" width={120} />
+      <Image
+        className={styles.jellybean}
+        src="/jellybean-button.svg"
+        width={120}
+        height={120}
+        alt={`${text} Button`}
+      />
     </button>
   );
 }
