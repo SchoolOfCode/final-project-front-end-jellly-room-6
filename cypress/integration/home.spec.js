d38a8407ea95a7334761354fee071fc637975001
YeganaAbd <abd.yegane@gmail.com>
2022-02-25 13:48:05 +0000

    Landing page tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,11 +4,12 @@ context("Home page", () => {
     cy.visit("http://localhost:3000");
   });
   it("Should find landing page", () => {
-    cy.get("h1").contains("JELLLY");
     cy.get("h2").contains(
       "The fun, free way to learn maths and improve financial literacy."
     );
+    cy.get("h1").contains("JELLLY");
     cy.get(".login").contains("Login");
-    cy.get("div[class={titleLogo}]").find("img").should("be.visible");
+    cy.get(".large").contains("Get Started");
+    cy.get("div").find("img").should("be.visible");
   });
 });
