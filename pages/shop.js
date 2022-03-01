a884bdbf3466a1028c99a14aa2f1740a4b419592
LTBurton1 <Liam.burton@hotmail.com>
2022-03-01 11:19:04 +0000

    Purchases working

--- pages/shop.js
+++ pages/shop.js
@@ -1,27 +1,49 @@
 import NavBar from "../src/components/NavBar";
 import ShopCategory from "../src/components/ShopCategory";
+import Loading from "../src/components/Loading";
 import getAuth0User from "../src/hooks/getAuth0User";
 import useUserInfo from "../src/hooks/useUserInfo";
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import InformationCard from "../src/components/InformationCard";
 import style from "../styles/shop.module.css";
+import { useEffect, useState } from "react";
 
-const data = [
-  { id: 1, src: "/logoJelly.png", alt: "beans-coins", price: 150 },
-  { id: 1, src: "/jelly_guy_blue.png", alt: "beans-coins", price: 150 },
-  { id: 1, src: "/jelly_guy_green.png", alt: "beans-coins", price: 150 },
-  { id: 1, src: "/jelly_guy_red.png", alt: "beans-coins", price: 150 },
+const items = [
+  { id: 1, purchase_name: "blue", src: "/jelly_guy_blue.png", alt: "blue-bean", price: 1 },
+  { id: 2, purchase_name: "green", src: "/jelly_guy_green.png", alt: "green-bean", price: 1 },
+  { id: 3, purchase_name: "red", src: "/jelly_guy_red.png", alt: "red-bean", price: 1 },
 ];
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Shop({ auth0User }) {
   const { user, error, isLoading } = useUser();
   const userInfo = useUserInfo(auth0User.username);
-  console.log(userInfo);
-  if (isLoading) return <div>Loading...</div>;
+  const [beans, setBeans] = useState("");
+
+  useEffect(() => {
+    setBeans(userInfo.beans);
+  }, [userInfo]);
+
+  if (isLoading) return <Loading />;
   if (error) return <div>{error.message}</div>;
-  // if (!user) {
-  //   window.location.href = "/";
-  // }
+
+  console.log(userInfo);
+
+  async function updateBeans(price) {
+    // Update beans in database
+    const body = { XP: 0, beans: -price };
+    await fetch(`${API_URL}/users/${userInfo.user_id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    // Update beans state for front end display
+    setBeans(beans - price);
+  }
 
   return (
     user && (
@@ -31,10 +53,13 @@ export default function Shop({ auth0User }) {
           <h1>The Jellly Shop</h1>
         </div>
 
-        <InformationCard username={userInfo.username} beans={userInfo.beans} />
-        <ShopCategory categorytitle="Color skins" data={data} />
-        <ShopCategory categorytitle="Color skins 2" data={data} />
-        <ShopCategory categorytitle="Color skins 3" data={data} />
+        <InformationCard username={userInfo.username} beans={beans} />
+        <ShopCategory
+          user={userInfo}
+          categorytitle="Color skins"
+          items={items}
+          updateBeans={updateBeans}
+        />
       </>
     )
   );
--- src/components/ShopItem/ShopItem.js
+++ src/components/ShopItem/ShopItem.js
@@ -1,18 +1,50 @@
 import Image from "next/image";
-import Button from "react-bootstrap/Button"
-import style from "../../../styles/shop.module.css"
+import Button from "react-bootstrap/Button";
+import style from "../../../styles/shop.module.css";
+import useUserInfo from "../../hooks/useUserInfo";
 
-export default function ShopItem({ src, alt, price }) {
-    
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export default function ShopItem({ item, user, updateBeans }) {
+  const { purchase_name, src, alt, price } = item;
+
+  async function addPurchase(purchase_name) {
+    const body = {
+      purchase_name,
+      user_id: userInfo.user_id,
+    };
+
+    const res = await fetch(`${API_URL}/purchases`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await res.json();
+
+    console.log(data);
+  }
+
+  function handlePurchase() {
+    // Check if user has enough beans
+    if (user.beans < item.price) return;
+    // If they do, remove beans from user in db
+    updateBeans(price);
+
+    //
+  }
+
   return (
     <div className={style.shopitem}>
       <Image src={src} width={50} height={100} alt={alt} />
       <div className={style.price}>
-      <p className={style.tagprice}> {price}</p>
-      <Image src="/beansCoins.png" width={50} height={2} alt="beans-coins" />
+        <p className={style.tagprice}> {price}</p>
+        <Image src="/beansCoins.png" width={50} height={2} alt="beans-coins" />
       </div>
-      <Button>Buy</Button>
+      <Button onClick={handlePurchase}>Buy</Button>
     </div>
   );
 }
-
