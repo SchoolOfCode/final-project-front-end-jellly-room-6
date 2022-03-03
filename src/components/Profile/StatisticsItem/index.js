4c11c242761a39e571b25db7e84b382dd2b8b631
YeganaAbd <abd.yegane@gmail.com>
2022-03-03 11:36:18 +0000

    profile responsive

--- src/components/Profile/StatisticsItem/index.js
+++ src/components/Profile/StatisticsItem/index.js
@@ -3,7 +3,7 @@ import styles from "./statistics.module.css";
 export default function StatisticsItem({ title, value }) {
   return (
     <div className="statistics-item">
-      <h3>{title}</h3>
+      <h3 className={styles.title}>{title}</h3>
       <div className={styles.statisticsValue}>{value}</div>
     </div>
   );
