70260a8837afa64bc85f2a7c651ba3a98b77dc19
Laurence279 <lnunn279@gmail.com>
2022-03-04 15:12:24 +0000

    increase question count

--- pages/question.js
+++ pages/question.js
@@ -176,9 +176,11 @@ export const getServerSideProps = withPageAuthRequired({
     function getQuestions(arr) {
       if (!arr)
         throw new Error(`API did not return any questions for category: ${ctx.query.category}`);
+
+
       return shuffleArray(data.payload).slice(0, questionCount);
     }
-    const questionCount = 4;
+    const questionCount = 5;
     function shuffleArray(arr) {
       return arr.sort(() => Math.random() - 0.5);
     }
