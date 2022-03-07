aa5fd638f50547d8835dd4892eb1a65e8d8f308c
shuamp <castel.davila@gmail.com>
2022-03-07 13:42:53 +0000

    new jelly images

--- src/data.js
+++ src/data.js
@@ -4,6 +4,7 @@ export const items = [
     purchase_name: "blue",
     src: "/jelly_guy_blue.png",
     alt: "blue-bean",
+    category: "skins",
     price: 20,
   },
   {
@@ -11,6 +12,7 @@ export const items = [
     purchase_name: "green",
     src: "/jelly_guy_green.png",
     alt: "green-bean",
+    category: "skins",
     price: 20,
   },
   {
@@ -18,6 +20,7 @@ export const items = [
     purchase_name: "red",
     src: "/jelly_guy_red.png",
     alt: "red-bean",
+    category: "skins",
     price: 20,
   },
   {
@@ -25,8 +28,58 @@ export const items = [
     purchase_name: "jellies",
     src: "/threeJellies.png",
     alt: "jellies",
+    category: "skins", //is it really a skin?
     price: 20,
   },
+  {
+    id: 5,
+    purchase_name: "Chef Hat",
+    src: "/chef_hat.png",
+    alt: "chef hat",
+    price: 35,
+    category: "hats",
+  },
+  {
+    id: 6,
+    purchase_name: "Sheriff Hat",
+    src: "/sheriff_hat_right.png",
+    alt: "sheriff hat",
+    price: 35,
+    category: "hats",
+  },
+  {
+    id: 7,
+    purchase_name: "Mexican Hat",
+    src: "/mexican_hat.png",
+    alt: "amigos mexican sombrero",
+    price: 35,
+    category: "hats",
+  },
+  {
+    id: 8,
+    purchase_name: "Meadow Landscape",
+    src: "/meadow_background.jpg",
+    alt: "a meadow landscape",
+    price: 35,
+    category: "backgrounds",
+  },
+  {
+    id: 9,
+    purchase_name: "Mars Landscape",
+    src: "/mars_background.jpg",
+    alt: "a Mars landscape",
+    price: 35,
+    category: "backgrounds",
+  },
+  {
+    id: 10,
+    purchase_name: "Space Landscape",
+    src: "/space_background.jpg",
+    alt: "a space landscape",
+    price: 35,
+    category: "backgrounds",
+  },
+  
 ];
 
 export const sections = [
@@ -38,7 +91,13 @@ export const sections = [
   {
     id: 2,
     name: "financial",
-    categories: ["Interest Rates", "Currency", "Mortgages", "Charity", "Savings"],
+    categories: [
+      "Interest Rates",
+      "Currency",
+      "Mortgages",
+      "Charity",
+      "Savings",
+    ],
   },
   {
     id: 3,
@@ -48,7 +107,13 @@ export const sections = [
   {
     id: 4,
     name: "general",
-    categories: ["Addition", "Subtraction", "Multiplication", "Division", "Further"],
+    categories: [
+      "Addition",
+      "Subtraction",
+      "Multiplication",
+      "Division",
+      "Further",
+    ],
   },
   {
     id: 5,
