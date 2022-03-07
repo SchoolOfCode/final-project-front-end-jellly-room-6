c6623aa20e7a30fe6c197c8384beb6c542a964b5
YeganaAbd <abd.yegane@gmail.com>
2022-03-07 15:42:44 +0000

    profile tests

--- cypress/integration/profile.spec.js
+++ cypress/integration/profile.spec.js
@@ -4,4 +4,18 @@ context("Profile page", () => {
       cy.visit("/profile");
     });
   });
+  it("Should find visible logo", () => {
+    cy.get("[data-cy=logo]").should("be.visible");
+  });
+  it("Should find username", () => {
+    cy.get("[data-cy=username]").contains("user");
+  });
+  it("Should find user email", () => {
+    cy.get("[data-cy=email]").contains("test@user.com");
+  });
+
+  it("Should find Statistics and Achievements of user", () => {
+    cy.get("[data-cy=title]").contains("Statistics");
+    cy.get("[data-cy=title]").contains("Achievements");
+  });
 });
