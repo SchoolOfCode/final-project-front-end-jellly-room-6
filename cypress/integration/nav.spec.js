ed52f1b1cce5f9ed4358137f10271400a54ae65c
YeganaAbd <abd.yegane@gmail.com>
2022-03-07 11:38:37 +0000

    navigation to shop page

--- cypress/integration/nav.spec.js
+++ cypress/integration/nav.spec.js
@@ -1,23 +1,21 @@
-// describe("Navbar", () => {
-//   context(" Resolution", () => {
-//     beforeEach(() => {
-//       cy.viewport(1100, 700);
-//     });
-//   });
+describe("Navbar", () => {
+  context(" Resolution", () => {
+    beforeEach(() => {
+      cy.viewport(1800, 1300);
+    });
 
-describe("When you visit home", () => {
-  it("should navigate to home page", () => {
-    cy.login().then((resp) => {
-      cy.visit("http://localhost:3000/home");
+    describe("When you visit home", () => {
+      it("should navigate to home page", () => {
+        cy.login().then((resp) => {
+          cy.visit("http://localhost:3000/home");
+        });
+      });
+      describe("Nav", () => {
+        it("should navigate to Shop page", () => {
+          cy.get("[data-cy=nav-item]").contains("Jelly Shop").click();
+          cy.url().should("include", "http://localhost:3000/shop");
+        });
+      });
     });
   });
-  // describe("Nav", () => {
-  //   it("should navigate to Shop page", () => {
-  //     cy.get("[data-cy=nav-item]")
-  //       .contains("http://localhost:3000/shop")
-  //       .click();
-  //     cy.url().should("include", "http://localhost:3000/shop");
-  //   });
-  // });
 });
-//});
--- src/components/NavDropdown/index.js
+++ src/components/NavDropdown/index.js
@@ -1,34 +1,39 @@
-import styles from "./index.module.css"
-import Link from "next/link"
-import useVisible from "../../hooks/useVisible"
+import styles from "./index.module.css";
+import Link from "next/link";
+import useVisible from "../../hooks/useVisible";
 
-export default function NavDropdown(){
-
+export default function NavDropdown() {
   const { ref, isVisible, setIsVisible } = useVisible(false);
-  
-    return <div onClick={e => setIsVisible(!isVisible)} className={`${styles.dropdownContainer}`}>
-    <p ref={ref}>...</p>
-    <div className={`${styles.dropdownMenu} ${isVisible && styles.show}` }   >
-    <div className={styles.dropdownMenuLinks}  >
-        <Link href="/home">
-          <a className={styles.link}  >Home</a>
-        </Link>
-        <Link href="/shop">
-          <a className={styles.link}  >Jelly Shop</a>
-        </Link>
-        <Link href="/leaderboard">
-          <a className={styles.link}  >Leaderboard</a>
-        </Link>
-        <Link href={"/profile"}>
-          <a className={styles.link}  >Profile</a>
-        </Link>
-      <Link  href="/api/auth/logout">
-        <a >
-          <button className={`${styles.logoutBtn} btn`}>Logout</button>
-        </a>
-      </Link>
+
+  return (
+    <div
+      onClick={(e) => setIsVisible(!isVisible)}
+      className={`${styles.dropdownContainer}`}
+    >
+      <p ref={ref}>...</p>
+      <div className={`${styles.dropdownMenu} ${isVisible && styles.show}`}>
+        <div className={styles.dropdownMenuLinks}>
+          <Link href="/home">
+            <a className={styles.link}>Home</a>
+          </Link>
+          <Link href="/shop">
+            <a className={styles.link}>
+              Jelly Shop
+            </a>
+          </Link>
+          <Link href="/leaderboard">
+            <a className={styles.link}>Leaderboard</a>
+          </Link>
+          <Link href={"/profile"}>
+            <a className={styles.link}>Profile</a>
+          </Link>
+          <Link href="/api/auth/logout">
+            <a>
+              <button className={`${styles.logoutBtn} btn`}>Logout</button>
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
-  </div> 
-
+  );
 }
