71a3e98a427435a0f2c490f842c8377a489e205b
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-03-09 11:32:24 +0000

    hometests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -5,4 +5,21 @@ context("Home page", () => {
       cy.visit("/home");
     });
   });
+  it("should get scroller", () => {
+    cy.get("[data-cy=scroller]").should("be.visible");
+  })
+
+  it("should display category name", () => {
+    cy.get("[data-cy=category]").contains("Addition");
+  })
+
+  it("should get jar of beans image", () => {
+    cy.get("[data-cy=jarofbeans]").should("be.visible");
+  })
+
+  it("should get jelly avatar image", () => {
+    cy.get("[data-cy=jellyavatar]").should("be.visible");
+  })
+
 });
+
